70f63520be0efbca5e49462b757f4c42c7ce35fd
SAMPATH <59731374+tsk129@users.noreply.github.com>
2022-01-02 21:22:04 +0530

    Create InputInt.java

---
+++ InputInt.java
@@ -0,0 +1,22 @@
+// In this challenge, you must read  integers from stdin and then print them to stdout. 
+// Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int num0 = scanner.nextInt();
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
+        System.out.println(num0);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
+
+// There are  lines of input, and each line contains a single integer.
